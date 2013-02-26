e2af4a0b52e1a299fae7edbfd41a38066539adf2
stephane.lallee@gmail.com <stephane.lallee@gmail.com@47514057-a044-4353-f3a5-7c8adca241c9>
2013-02-26 11:14:10 +0000

    missing files

---
+++ trunk/src/MMCM/RobotConnector/PartAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotConnector
+{
+    public class PartAnimation
+    {
+	   public struct Frame
+	   {
+		  public double timeStamp;
+		  public double[] posture;
+	   }
+
+	   public List<Frame> framestack = new List<Frame>();
+    }
+}
