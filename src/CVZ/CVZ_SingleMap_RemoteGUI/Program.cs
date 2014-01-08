9999602184ef40f11d952d54f0cd8cd93b04038d
stephane.lallee <stephane.lallee@gmail.com>
2014-01-08 16:09:21 +0000

    [mainLoop] localization

--- src/CVZ/CVZ_SingleMap_RemoteGUI/Program.cs
+++ src/CVZ/CVZ_SingleMap_RemoteGUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using CVZ_Core.GUI;
 
 namespace RemoteMMCMGui
 {
@@ -11,12 +12,39 @@ namespace RemoteMMCMGui
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+			//Parse the command line arguments
+			string mapName = "-1";
+			string mapType = "-1";
+			for (int i = 0; i < args.Count(); i++)
+            {
+                if (args[i] == "--mapName")
+                    mapName = args[i + 1];
+                if (args[i] == "--mapType")
+                    mapType = args[i + 1];;
+            }
+			
             Network.init();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-		  Application.Run(new AskName());
+			if (mapName == "-1" || mapType == "-1")
+		    	Application.Run(new AskName());
+			else
+			{
+				if (mapType == "mmcm")
+				{
+			    	MMCM_RemoteControlPanel remotePanel = new MMCM_RemoteControlPanel();
+                	while (!remotePanel.Initialise(mapName)) ;
+					Application.Run(remotePanel);//.Show();
+				}
+            	else if (mapType == "ctpc")
+            	{
+                	CTPC_RemoteControlPanel remotePanel = new CTPC_RemoteControlPanel();
+                	while (!remotePanel.Initialise(mapName)) ;
+					Application.Run(remotePanel);//.Show();
+            	}	
+			}
         }
     }
 }
